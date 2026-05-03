88f699c3c5a832ffc0f81b3cd92b1e3561224a8d
Big Ray <bigbigraydeng@gmail.com>
2026-05-04 03:56:20 +1200

    feat: integrate Google Tag Manager (GTM-MRW95G5Q)
    - Add GTM script initialization in <head> with beforeInteractive strategy
    - Add GTM noscript fallback in <body> for users without JavaScript
    - GTM configured to work alongside existing GA4 and Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -71,6 +71,20 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
       <head>
+        {/* Google Tag Manager */}
+        <Script
+          id="google-tag-manager"
+          strategy="beforeInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MRW95G5Q');
+            `,
+          }}
+        />
         {/* Google Analytics 4 */}
         {process.env.NEXT_PUBLIC_GA4_MEASUREMENT_ID && (
           <>
@@ -119,6 +133,15 @@ export default function RootLayout({
         </noscript>
       </head>
       <body className="font-sans antialiased">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MRW95G5Q"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <CookieConsentManager />
         <ConditionalChrome>{children}</ConditionalChrome>
       </body>
